442c88c786e8d4e998b794877195be061b5d8ccf
CJ Agwai <cja6637@rit.edu>
2025-06-04 14:43:00 -0400

    front edn and back end are linked

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntriesComponent } from './entries/entries.component';
+import { MessageComponent } from './message/message.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntriesComponent
+    EntriesComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/entries/entries.component.ts
+++ frontend/src/app/entries/entries.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Entry } from '../entry';
+import { EntryService } from '../entry.service';
 
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.component.html',
   styleUrl: './entries.component.css'
 })
-export class EntriesComponent {
-  entry : Entry = {
-    id: 1,
-    title: 'The Dark Side of the Moon',
-    artist: 'Pink Floyd',
-    year: '1973',
-    genre: 'Progressive Rock',
-    rating: 5,
-    listenDate: '1973-03-01',
-    review: 'A masterpiece of an album. It is a must-listen for any music lover.',
-    favoriteSong: 'Time'
+export class EntriesComponent implements OnInit {
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) { }
+
+  ngOnInit(): void {
+      this.getEntries();
   }
 
+  getEntries(): void {
+    this.entryService.getEntries()
+      .subscribe(entries => this.entries = entries);
+  }
 }
